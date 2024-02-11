104f88f98b4e89404ba07ba5b1b8439bb9ef27c6
Marta Bakešová <marta.bakesova@centrum.cz>
2024-02-11 13:21:24 +0100

    prirazeni

---
+++ script.js
@@ -0,0 +1,14 @@
+let fahrenheit = Number(prompt ("Zadejte stupně Fahrenheita: "));
+console.log ("fahrenheit " + fahrenheit);
+
+let celsius = Number((5* (fahrenheit - 32)) / 9);
+console.log ("celsia: " + celsius);
+
+document.body.innerHTML += "<p>" + "Váš převod je: " + "</p>";
+
+document.body.innerHTML += "<p>" + fahrenheit + " stupňů Fahrenheita " + "= " + celsius + " stupňů Celsia" + "</p>";
+
+let trouba = Math.round (celsius)
+console.log (trouba)
+
+document.body.innerHTML += "<p>" + "Na troubě nastavte " + trouba + " stupňů." + "</p>";
