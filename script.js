e8173e002e2e3c9f6108b508d6a86a8856f2ca3a
Marta Bakešová <marta.bakesova@centrum.cz>
2024-02-11 14:29:30 +0100

    uprava textu

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ console.log ("celsia: " + celsius);
 
 //document.body.innerHTML += "<p>" + "Váš převod je: " + "</p>";
 
-document.body.innerHTML += "<p>" + fahrenheit + " stupňů Fahrenheita " + "= " + celsius + " stupňů Celsia" + "</p>";
+document.body.innerHTML += "<p>"  + fahrenheit + " stupňů Fahrenheita " + "= " + celsius + " stupňů Celsia" + "</p>";
 
 let trouba = Math.round (celsius)
 console.log (trouba)
 
-document.body.innerHTML += "<p>" + "Na troubě nastavte " + trouba + " stupňů." + "</p>";
+document.body.innerHTML += "<p><h1>" + "Na troubě nastavte " + trouba + " stupňů." + "</h1></p>";
