ed4802750bd07de4574e7444f079c3f094bb464b
Marta Bakešová <marta.bakesova@centrum.cz>
2024-02-11 13:34:05 +0100

    nadpis

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ console.log ("fahrenheit " + fahrenheit);
 let celsius = Number((5* (fahrenheit - 32)) / 9);
 console.log ("celsia: " + celsius);
 
-document.body.innerHTML += "<p>" + "Váš převod je: " + "</p>";
+//document.body.innerHTML += "<p>" + "Váš převod je: " + "</p>";
 
 document.body.innerHTML += "<p>" + fahrenheit + " stupňů Fahrenheita " + "= " + celsius + " stupňů Celsia" + "</p>";
 
